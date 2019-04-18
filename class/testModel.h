b1608f7508d90506b6f2b9aee9f8d7f7c52fa2a2
mhj <605370846@qq.com>
2019-04-18 17:22:14 +0800

    1111

---
+++ class/testModel.h
@@ -0,0 +1,17 @@
+//
+//  testModel.h
+//  test
+//
+//  Created by 马恒健 on 2019/4/18.
+//  Copyright © 2019 mahengjian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface testModel : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
